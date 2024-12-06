71961f53d8122879dd1c96500274f5eddea10182
Avanish Singh <132077441+panthera-leo091@users.noreply.github.com>
2024-12-06 11:34:50 +0000

    datatype

---
+++ basics/dataTypes.js
@@ -0,0 +1,30 @@
+"use strict";
+// Treat all js code as newer version
+
+// alert("hello");
+//we are using nodejs, not browser
+
+// console.log(3+3);
+// console.log("Avanish");
+
+
+let name = "Avanish";
+let age = 23;
+let isLoggedIn = false;
+
+
+// number(range(2**53))
+// bigint
+// String("val" or 'val) prefer "val";
+// Boolean => true/false
+// null => standalone value
+// undefine => var without value but declered
+// symbol => used to define uniqueness
+
+
+// object
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof(isLoggedIn));
+console.table([typeof null, typeof undefined]);
---
+++ basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553;
+let accountEmail = "ava@google.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+let accountState;
+
+// accountId = 121323;   // not allowed
+console.log(accountId);
+
+accountEmail = "avanish@algo.com";
+accountPassword = "2121212";
+accountCity = "Delhi";
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
