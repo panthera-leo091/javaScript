12745306f9bb6e85ff3c579bed497ece9d8890a6
Avanish Singh <132077441+panthera-leo091@users.noreply.github.com>
2024-12-09 04:06:14 +0000

    temp

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -4,3 +4,10 @@ console.log(typeof(score));
 
 let valInNumber = Number(score);
 console.log(typeof valInNumber);
+
+let nan = "33as";
+let stringToNaN = Number(nan);
+
+console.log(stringToNaN);
+console.log(typeof stringToNaN);
+console.log(nan);
