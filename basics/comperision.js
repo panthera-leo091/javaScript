30eacead3f1cc875d0c1d44f5e843ed3e6f79503
Avanish Singh <132077441+panthera-leo091@users.noreply.github.com>
2024-12-10 07:22:02 +0000

    save

---
+++ basics/comperision.js
@@ -0,0 +1,29 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+// console.log("new Line");
+// console.log(1 > 2);
+// console.log(1 < 2);
+// console.log(1 >= 2);
+// console.log(1 <= 2);
+// console.log(1 == 2);
+// console.log(1 != 2);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+// console.log("2" == 2);  // true
+// console.log("2" === 2);  // false
+// console.log("2" != 2);
+
+// console.log(null > 0);   // comperison ops like >, <, >=, <=, etc convert null to number and compaire
+// console.log(null == 0);  // equality check(==) work differently and doesn't convert null to number
+// console.log(null >= 0);  // here null is converted to 0
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined >= 0);
+// console.log(undefined == 0);
+// console.log(undefined === 0);
