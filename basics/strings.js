c5766ca2900f10238af0492ec812dc29650f06a3
Avanish Singh <132077441+panthera-leo091@users.noreply.github.com>
2024-12-10 10:03:39 +0000

    save

---
+++ basics/strings.js
@@ -0,0 +1,7 @@
+const name = "Dhron";
+const role = new String("Teacher");
+const son = "Aswadhama";
+
+// console.log("In mahabharat " + name + " was the " + role + " of hasthinapur prince and his son " + son);
+
+console.log(`In mahabharat ${name} was the ${role} of hasthinapur prince and his son ${son}`)  // string interpolation
