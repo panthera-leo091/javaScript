f246d90e63adeb7e80dab1bbb2a6fd6dd704ae50
Avanish Singh <132077441+panthera-leo091@users.noreply.github.com>
2024-12-10 09:36:46 +0000

    sava

--- basics/dataTypes-summary.js
+++ basics/dataTypes-summary.js
@@ -25,7 +25,7 @@ const bigNum = 21548798653254886n;
 
 // Array, Objects, Functions
 
-const heros = ["shaktiman", "naagraj", "daga", "Kirmada", "dushasana"];
+const heros = ["shaktiman", "naagraj", "doga", "Kirmada", "dushasana"];
 let obj = {id: 1, name: "dushasana", age: "dead", hero: true}
 console.log(obj);
 
@@ -34,3 +34,34 @@ const myFunction = function(){
 }
 
 console.table([typeof(heros), typeof(obj), typeof(myFunction)]);
+
+
+
+console.clear();
+
+
+//  +++++++++++++++++++++++++++++++
+
+// Stack (premitive), Heap(Non Premitive/referenced)
+
+let userName1 = "Shaktiman";   //stack
+let userName2 = userName1;    //stack
+userName2 = "Kirmada";
+console.log(userName1);
+console.log(userName2);
+
+
+console.clear();
+
+
+let userOne = {
+    name: "Dhritarashtra",
+    age: "Mrit",
+    hero: true
+}
+
+let userTwo = userOne;
+userTwo.age = "dead";
+
+console.log(userOne);
+console.log(userTwo);
