7946dfe9ee089d3ca17f8a0f396bf6cef98e6db5
Avanish Singh <132077441+panthera-leo091@users.noreply.github.com>
2024-12-10 06:47:22 +0000

    comit

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -51,3 +51,63 @@ console.table(
 let arr1 = [], arr2 = [1, 2, 3];
 console.log(Boolean(arr1));
 console.log(Boolean(arr2));
+
+
+/***********************************************************************          Operatiors          *******************************************************************************/
+
+
+/*
+let val = 6;
+let negVal = -val;
+console.log(negVal);
+
+console.log(val+val);
+console.log(val-val);
+console.log(val*val);
+console.log(val/val);
+console.log(val%val);
+console.log(val**val);
+
+*/
+
+
+
+// let s1 = "Hello";
+// let s2 = " ";
+// let s3 = "World";
+// let s4 = "!";
+
+// let resString = s1 + s2 + s3 + s4;
+
+// console.log(resString);
+
+// let str1 = "12";
+// let num1 = 11, num2 = 89;
+// console.log(str1 + num);
+// console.log(typeof(str1 + num));
+// console.log(num + str1);
+// console.log(typeof(num + str1));
+// console.log(1 - "12")   // => -11
+// console.log(1 - "a")    // => NaN
+
+// console.log(num1 + num2 + str1);
+// console.log(num1 + str1 + num2);
+// console.log(str1 + num1 + num2);
+
+// console.log(true);
+// console.log(+true);
+// console.log(true+0);
+// console.log(+"");
+// console.log(typeof(+""));
+// console.log("" + 0);
+// console.log(typeof("" + 0));
+
+
+
+// let num1, num2, num3;
+// num1 = num2 = num3 = 7;
+// console.table([num1, num2, num3]);
+// num1 += 1;
+// num2 += 2;
+// num3 += 3;
+// console.table([num1, num2, num3]);
